36d0ddbf528abc4c56084eb469a47a4d12783a1e
Hugo Zul <hugozul@Hugos-MacBook-Pro.local>
2016-08-03 16:56:16 -0700

    second iteration

--- slowReads.go
+++ slowReads.go
@@ -21,11 +21,18 @@ import (
 */
 
 
+//log reads
+	//e^normal dist
+	// rel between sleep time and time to axquire 
 
 func check(e error) {
     if e != nil {
         panic(e)
     }
+}
+type readData struct {
+	timetoAcquire time.Duration
+	sleepTime time.Duration
 }
 
 func main(){
@@ -34,20 +41,26 @@ func main(){
 
     writeticker := time.NewTicker(time.Second) //write every second
 	quit := make(chan struct{})
-	durationOfExperiment := 30
+	durationOfExperiment := 60
 	readers := 5
 
-	durations := make(chan time.Duration, durationOfExperiment)
+	writeTimetoAcquire := make(chan time.Duration, durationOfExperiment)
+	rData := make(chan readData, 10000)
+
 
 	/*************************** n readers with random sleep time*********************/
 	for i := 0; i<readers; i++{
 		go func(i int) {
 		    for {
+		    	sleepTime := time.Duration(math.Exp(rand.NormFloat64()))
+		    	fmt.Println("SLEEPYTIME: ", sleepTime)
+		    	now := time.Now()
 	        	lock.RLock()
-		        multiplier := time.Duration(math.Abs(rand.NormFloat64()))
-		        time.Sleep(time.Second * multiplier)
+	        	timetoAcquire := time.Since(now)
+		        time.Sleep(time.Second * sleepTime)
 		        lock.RUnlock()
 
+		        rData <- readData{timetoAcquire, sleepTime}
 		        runtime.Gosched()
 		    }
 		 }(i)
@@ -60,53 +73,88 @@ func main(){
 				case <- writeticker.C:
 					now := time.Now()
 					lock.Lock()
+					timetoAcquire := time.Since(now)
 					lock.Unlock()
-					duration := time.Since(now)
-					
-					durations <- duration
+						
+					writeTimetoAcquire <- timetoAcquire
 
 				case <- quit:
 					fmt.Println("I AM HERE")
 					writeticker.Stop()
-					durations <- time.Duration(0)
+					writeTimetoAcquire <- time.Duration(0)
 					return
 			}
 		}
 	}()
 
-	/****************recording happens hear***************/
-	f, err := os.OpenFile("data.txt", os.O_RDWR|os.O_APPEND, 0666)
+	/****************recording happens here***************/
+	f, err := os.OpenFile("writeData.txt", os.O_RDWR|os.O_APPEND, 0666)
+	check(err)
 	dataHeader := "\n\nNumber of readers: " + strconv.Itoa(readers) + "\n"
 	f.WriteString(dataHeader)
-    check(err)
+
+	ff, err := os.OpenFile("readData.txt", os.O_RDWR|os.O_APPEND,0666)
+	check(err)
+	ff.WriteString(dataHeader)
+
+	sleepTimes := make([]time.Duration,0)
+
     defer f.Close()
+    defer ff.Close()
 
     writes := 0
-	durationAverage := time.Duration(time.Second*0)
+    reads := 0
 
+    //write recorder
 	go func(){
 		for {
 			select {
 			case <- quit:
 				return
 			default:
-				duration := <-durations
-				fmt.Println("Duration of write: ", duration)
-	    		writes+=1
-	    		durationAverage +=duration
-	    		_, err := f.WriteString(duration.String()+"\n")
+				tta := <-writeTimetoAcquire
+				fmt.Println("Time to acquire write: ", tta)
+	    		writes += 1
+	    		_, err := f.WriteString(tta.String()+"\n")
 	    		check(err)
 			}
 
     	}
 	}()
+
+	//read recorder
+
+	go func(){
+		for{
+			select{
+			case <-quit:
+				return
+			default:
+				readData := <-rData
+				tta := readData.timetoAcquire
+				st := readData.sleepTime
+				sleepTimes = append(sleepTimes, st)
+				reads += 1
+				_, err := ff.WriteString(tta.String()+"\n")
+				check(err)
+			}
+		}
+	}()
 	
-	/*****************Upon finishing the experiment***********/
-    time.Sleep(time.Second*time.Duration(durationOfExperiment)) 
+	time.Sleep(time.Second*time.Duration(durationOfExperiment)) 
     close(quit)
 
-    durationAverage= time.Duration(time.Duration(durationAverage.Seconds()/float64(writes)) * time.Second)
-    _,err = f.WriteString("Number of writes: " + strconv.Itoa(writes) + "\n"+ "Duration Average: " + durationAverage.String())
+    	/*****************Upon finishing the experiment***********/
+    _,err = ff.WriteString("\n **SleepTimes \n")
+    check(err)
+    for i := 0; i<len(sleepTimes); i++{
+    	_,err = ff.WriteString(sleepTimes[i].String()+ "\n")
+    	check(err)
+    }
+
+    _,err = f.WriteString("Number of writes: " + strconv.Itoa(writes) + "\n")
+    check(err)
+    _,err = ff.WriteString("Number of reads: " + strconv.Itoa(reads) + "\n")
     check(err)
 
     fmt.Println("DONE")
